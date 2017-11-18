c74152533e4335d24f7085113e2823681c847676
Wilson <wilsonvargas_6@outlook.com>
2017-11-18 17:18:08 -0500

    Added support for iOS

--- src/CustomIcon/CustomIcon/CustomIcon.iOS/AppDelegate.cs
+++ src/CustomIcon/CustomIcon/CustomIcon.iOS/AppDelegate.cs
@@ -24,7 +24,6 @@ namespace CustomIcon.iOS
         {
             global::Xamarin.Forms.Forms.Init();
             LoadApplication(new App());
-
             return base.FinishedLaunching(app, options);
         }
     }
---
+++ src/CustomIcon/CustomIcon/CustomIcon.iOS/IconNavigationPageRenderer.cs
@@ -0,0 +1,41 @@
+﻿using CoreGraphics;
+using CustomIcon.iOS;
+using CustomIcon.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CustomIcon.Views.MainPage), typeof(IconNavigationPageRenderer))]
+namespace CustomIcon.iOS
+{
+    public class IconNavigationPageRenderer : PhoneMasterDetailRenderer
+    {
+        public override void ViewDidLayoutSubviews()
+        {
+            base.ViewDidLayoutSubviews();
+            SetupLeftButton();
+        }
+
+        void SetupLeftButton()
+        {
+            if (!(Element is MainPage mdp)) return;
+            if (!(Platform.GetRenderer(mdp.Detail) is UINavigationController nc)) return;
+
+            UIButton btn = new UIButton(UIButtonType.Custom);
+            btn.SetTitleColor(btn.TintColor, UIControlState.Normal);
+
+            btn.Frame = new CGRect(0, 0, 50, 44);
+            var img = UIImage.FromFile("Images/newIcon.png");
+            btn.SetTitle(string.Empty, UIControlState.Normal);
+            btn.SetImage(img, UIControlState.Normal);
+            btn.TouchUpInside += (sender, e) => mdp.IsPresented = true;
+            var lbbi = new UIBarButtonItem(btn);
+            nc.NavigationBar.TopItem.LeftBarButtonItem = lbbi;
+        }
+
+
+    }
+}
